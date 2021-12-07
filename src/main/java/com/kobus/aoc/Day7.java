d0ec6d782866f7b4b870673776170fcb992c7bfd
Kobus Pretorius <kobus.pretorius@partner.bmw.co.za>
2021-12-07 07:55:52 +0200

    Day 7 solution

--- src/main/java/com/kobus/aoc/Day7.java
+++ src/main/java/com/kobus/aoc/Day7.java
@@ -1,6 +1,11 @@
 package com.kobus.aoc;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Supplier;
+import java.util.stream.IntStream;
 
 /**
  * Advent of Code 2021 Solutions
@@ -22,11 +27,71 @@ public class Day7 extends AoCRunnable {
 
     @Override
     public String part1() {
-        return "";
+        List<Integer> crabs = new ArrayList<>(Arrays.stream(input.get(0).split(",")).mapToInt(Integer::parseInt).boxed().toList());
+
+        int min = Integer.MAX_VALUE;
+        int max = 0;
+        for (Integer i : crabs) {
+            if (min > i) {
+                min = i;
+            }
+            if (max < i) {
+                max = i;
+            }
+        }
+        Supplier<IntStream> sup = () -> crabs.stream().mapToInt(Integer::intValue);
+
+        int minFuelCost = -1;
+        for (int i = min; i <= max; i++) {
+
+            int finalI = i;
+            int fuelCost = sup.get().map(c -> Math.abs(finalI - c)).sum();
+            if (minFuelCost == -1 || fuelCost < minFuelCost) {
+                minFuelCost = fuelCost;
+            }
+        }
+
+        return "" + minFuelCost;
     }
 
     @Override
     public String part2() {
-        return "";
+        List<Integer> crabs = new ArrayList<>(Arrays.stream(input.get(0).split(",")).mapToInt(Integer::parseInt).boxed().toList());
+
+        int min = Integer.MAX_VALUE;
+        int max = 0;
+        for (Integer i : crabs) {
+            if (min > i) {
+                min = i;
+            }
+            if (max < i) {
+                max = i;
+            }
+        }
+        List<Integer> cachedDistances = precalculateDistance(max);
+        Supplier<IntStream> sup = () -> crabs.stream().mapToInt(Integer::intValue);
+
+        int minFuelCost = -1;
+        for (int i = min; i <= max; i++) {
+
+            int finalI = i;
+            int fuelCost = sup.get().map(c -> cachedDistances.get(Math.abs(finalI - c))).sum();
+            if (minFuelCost == -1 || fuelCost < minFuelCost) {
+                minFuelCost = fuelCost;
+            }
+        }
+
+        return "" + minFuelCost;
+    }
+
+    public List<Integer> precalculateDistance(int max) {
+        var precalcDistances = new ArrayList<Integer>();
+        precalcDistances.add(0);
+        int sum = 0;
+        for (int n = 1; n <= max; n++) {
+            sum += n;
+            precalcDistances.add(sum);
+        }
+        return precalcDistances;
     }
 }
---
+++ src/main/java/com/kobus/aoc/Day8.java
@@ -0,0 +1,37 @@
+package com.kobus.aoc;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Supplier;
+import java.util.stream.IntStream;
+
+/**
+ * Advent of Code 2021 Solutions
+ * Day 8
+ *
+ * @author Kobus Pretorius
+ */
+public class Day8 extends AoCRunnable {
+
+    public static void main(String[] args) throws IOException {
+        new Day8("8").run(true);
+        new Day8("8").run(false);
+    }
+
+    public Day8(String dayNumber) {
+        super(dayNumber);
+        debugging = true;
+    }
+
+    @Override
+    public String part1() {
+        return "";
+    }
+
+    @Override
+    public String part2() {
+        return "";
+    }
+}
